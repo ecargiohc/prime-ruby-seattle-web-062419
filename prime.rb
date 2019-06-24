1449d6c2c43f39891fcad1841396d9292aad01b2
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:41:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 def prime?(integer)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
-  true
+  # if integer.prime?
+    for integer in 2..integer-1
+    if integer %  == 0
+      false
+    
+  else 
+   true
+  end
+end
 end
