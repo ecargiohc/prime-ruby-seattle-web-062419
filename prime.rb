7ee4f65fc5dd4d2a86ebc9634c82e23ac2e32fd6
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:45:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   if integer < 2
     return false
   end
-    range = 2..integer - 1
+  range = 2..integer - 1
       range.each do |n|
         if (integer % n) == 0
             return false
