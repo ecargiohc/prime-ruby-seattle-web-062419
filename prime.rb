07bdae3fc0dc777c91c024a83e3458c2e40ff55c
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:39:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@ def prime?(integer)
   # if integer.prime?
     for integer in 2..integer-1
     if integer %  == 0
-      true
+      false
     
   else 
-    return false
+   true
   end
 end
 end
