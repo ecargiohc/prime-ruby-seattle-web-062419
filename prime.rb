f84f19b09a77db8b5687653990e960606ce2b682
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:43:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(integer)
-    return false if integer < 2
-    (2..integer - 1).each do |n|
-        if (integer % n) == 0
-            return false
-        end
-    end
+ (2..integer - 1).each do |x|
+  if (integer % x) == 0
+   return false
+  else
    return true
+  end
+end
 end
